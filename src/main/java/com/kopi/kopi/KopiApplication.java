179e74d4a55bca22f036b84e2b61df2ed0fe99dc
Khoi <nguyenngockhoi273@gmail.com>
2025-09-18 14:35:20 +0700

    Initial commit - setup environment

---
+++ src/main/java/com/kopi/kopi/KopiApplication.java
@@ -0,0 +1,13 @@
+package com.kopi.kopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kopi/kopi/KopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopi.kopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
